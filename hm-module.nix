f934a839b1f48223695fe3d2446d577fd43d7088
Benjamin Kaylor <blkaylor22@gmail.com>
2024-07-22 16:48:50 -0400

    vesktop enabled

--- hm-module.nix
+++ hm-module.nix
@@ -27,34 +27,50 @@ let
 in {
   options.programs.nixcord = {
     enable = mkEnableOption "Enables Discord with Vencord";
+    discord.enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Whether to enable discord
+        Disable to only install Vesktop
+      '';
+    };
+    vesktop.enable = mkEnableOption ''
+      Whether to enable Vesktop
+    '';
     package = mkOption {
       type = types.package;
       default = pkgs.discord;
-      defaultText = literalExpression "pkgs.discord";
-      example = literalExpression ''
-        pkgs.vesktop
-      '';
       description = ''
-        The Discord package to use. If using Vesktop instead of
-        Discord + Vencord make sure to disable vencord and
-        openASAR since they are already part of Vesktop package.
+        The Discord package to use
+      '';
+    };
+    vesktopPackage = mkOption {
+      type = types.package;
+      default = pkgs.vesktop;
+      description = ''
+        The Vesktop package to use
       '';
     };
     configDir = mkOption {
       type = types.path;
       default = "${config.xdg.configHome}/Vencord";
       description = "Vencord config directory";
-      example = literalExpression "home/<user>/.config/Vencord";
+    };
+    vesktopConfigDir = mkOption {
+      type = types.path;
+      default = "${config.xdg.configHome}/vesktop";
+      description = "Config path for vesktop";
     };
     vencord.enable = mkOption {
       type = types.bool;
       default = true;
-      description = "Enable Vencord";
+      description = "Enable Vencord (for non-vesktop)";
     };
     openASAR.enable = mkOption {
       type = types.bool;
       default = true;
-      description = "Enable OpenASAR";
+      description = "Enable OpenASAR (for non-vesktop)";
     };
     quickCss = mkOption {
       type = types.str;
@@ -83,6 +99,14 @@ in {
       disableMinSize = mkEnableOption "Disable minimum window size for client";
       plugins = import ./plugins.nix { inherit lib; };
     };
+    vesktopConfig = mkOption {
+      type = with types; nullOr attrs;
+      default = null;
+      description = ''
+        Options to override vencord config for Vesktop. Use to set different
+        settings between configs
+      '';
+    };
     extraConfig = mkOption {
       type = with types; nullOr attrs;
       default = null;
@@ -93,21 +117,36 @@ in {
   config = mkIf cfg.enable (mkMerge [
     {
       home.packages = [
-        (cfg.package.override {
+        (mkIf cfg.discord.enable cfg.package.override {
           withVencord = cfg.vencord.enable;
           withOpenASAR = cfg.openASAR.enable;
         })
+        (mkIf cfg.vesktop.enable cfg.vesktopPackage)
       ];
     }
-    {
-      home.file."${cfg.configDir}/settings/quickCss.css".text = cfg.quickCss;
-    }
-    (mkIfElse (!builtins.isNull cfg.extraConfig) {
-      home.file."${cfg.configDir}/settings/settings.json".text =
-        builtins.toJSON (mkVencordCfg (cfg.config // cfg.extraConfig));
-    } {
-      home.file."${cfg.configDir}/settings/settings.json".text =
-        builtins.toJSON (mkVencordCfg cfg.config);
-    })
+    (mkIf cfg.discord.enable (mkMerge [
+      {
+        home.file."${cfg.configDir}/settings/quickCss.css".text = cfg.quickCss;
+      }
+      (mkIfElse (!builtins.isNull cfg.extraConfig) {
+        home.file."${cfg.configDir}/settings/settings.json".text =
+          builtins.toJSON (mkVencordCfg (cfg.config // cfg.extraConfig));
+      } {
+        home.file."${cfg.configDir}/settings/settings.json".text =
+          builtins.toJSON (mkVencordCfg cfg.config);
+      })
+    ]))
+    (mkIf cfg.vesktop.enable mkMerge [
+      {
+        home.file."${cfg.vesktopConfigDir}/settings/quickCss.css".text = cfg.quickCss;
+      }
+      (mkIfElse (!builtins.isNull cfg.vesktopConfig) {
+        home.file."${cfg.vesktopConfigDir}/settings/settings.json".text =
+          builtins.toJSON (mkVencordCfg (cfg.config // cfg.vesktopConfig));
+      } {
+        home.file."${cfg.vesktopConfigDir}/settings/settings.json".text =
+          builtins.toJSON (mkVencordCfg cfg.config);
+      })
+    ])
   ]);
 }
