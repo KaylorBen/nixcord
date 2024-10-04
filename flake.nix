505c964fef5a4c911d063b947c04df637d8c1c54
Benjamin Kaylor <blkaylor22@gmail.com>
2024-10-03 19:53:19 -0600

    move things around

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-  outputs = { self, nixpkgs }:
-    {
-      homeManagerModules.nixcord = import ./hm-module.nix;
-    };
+  outputs = { homeManagerModules.nixcord = import ./hm-module.nix; };
 }
