58df4aef95ba3cbd3e9a2a12254d3ac094d88f17
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-31 10:59:54 +1300

    added vencordPkgs as a input

--- hm-module.nix
+++ hm-module.nix
@@ -132,8 +132,7 @@ let
 
       # Check for a Nix expression and build if present
       buildIfExists = if builtins.pathExists "${fullPath}/default.nix" || builtins.pathExists "${fullPath}/shell.nix" then
-        import fullPath { inherit pkgs patchedVencord; }
-
+        import fullPath { inherit pkgs vencordPkgs patchedVencord; }
       else
         pluginDir;
     in
