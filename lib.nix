97a97d0ac043740433dfa607b3395f2ba5f928e2
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-11-14 23:08:49 +0200

    lib.nix: format with `nixfmt-rfc-style`

--- lib.nix
+++ lib.nix
@@ -10,7 +10,8 @@ let
     nameValuePair
     ;
 
-  upperNames = [  # these are option names to become UPPER_SNAKE_CASE
+  upperNames = [
+    # these are option names to become UPPER_SNAKE_CASE
     "webhook"
     "owner"
     "administrator"
@@ -29,14 +30,16 @@ let
     "minimizeToTray"
   ] ++ parseRules.upperNames;
 
-  lowerPluginTitles = [ # these are the only plugins with lowercase names in json
+  lowerPluginTitles = [
+    # these are the only plugins with lowercase names in json
     "iLoveSpam"
     "petpet"
   ] ++ parseRules.lowerPluginTitles;
 
   # this is since many json options in Vencord are just int values with no description
   # ALL OF THESE HAVE TO BE UNIQUE!!!
-  zeroOptions = [       # options which evaluate to int 0
+  zeroOptions = [
+    # options which evaluate to int 0
     "randomCharacters"
     "never"
     "playing"
@@ -52,7 +55,8 @@ let
     "plain"
     "whitelist"
   ] ++ parseRules.fakeEnums.zero;
-  oneOptions = [       # options which evaluate to int 1
+  oneOptions = [
+    # options which evaluate to int 1
     "consistent"
     "always"
     "streaming"
@@ -72,7 +76,8 @@ let
     "animatedDots"
     "blacklist"
   ] ++ parseRules.fakeEnums.one;
-  twoOptions = [       # options which evaluate to int 2
+  twoOptions = [
+    # options which evaluate to int 2
     "timestamp"
     "moreThanOne"
     "listening"
@@ -86,11 +91,12 @@ let
     "roomy"
     "avatars"
   ] ++ parseRules.fakeEnums.two;
-  threeOptions = [       # options which evaluate to int 3
+  threeOptions = [
+    # options which evaluate to int 3
     "watching"
     "customTime"
     "serverDefault"
-    "both"    # This references 2 options that both = 3 in JSON
+    "both" # This references 2 options that both = 3 in JSON
   ] ++ parseRules.fakeEnums.three;
   fourOptions = [
     "competing"
@@ -98,53 +104,81 @@ let
 
   isLowerCase = s: strings.toLower s == s;
 
-  unNixify = nixName: (
-    strings.toUpper (strings.concatStrings (builtins.map (char:
-      if (isLowerCase char) then char
-      else "_" + char
-    ) (strings.stringToCharacters nixName)
-  )));
+  unNixify =
+    nixName:
+    (strings.toUpper (
+      strings.concatStrings (
+        builtins.map (char: if (isLowerCase char) then char else "_" + char) (
+          strings.stringToCharacters nixName
+        )
+      )
+    ));
 
-  isLowerCamel = string: (
-    isLowerCase (builtins.substring 0 1 string)
-  );
+  isLowerCamel = string: (isLowerCase (builtins.substring 0 1 string));
 
-  toUpper = string: (
-    strings.concatStrings [
+  toUpper =
+    string:
+    (strings.concatStrings [
       (strings.toUpper (builtins.substring 0 1 string))
       (builtins.substring 1 (builtins.stringLength string) string)
-    ]
-  );
+    ]);
 
-  mkVencordCfg = cfg:
-  let
-    recurse = mapAttrs' (name: value: nameValuePair
-      (
-        # probably some kind of map function is better than this
-        if name == "enable" then "enabled" else
-        if name == "tagSettings" then "tagSettings" else  # the only name that = attrset not in upperNames
-        if name == "nsfwGateBypass" then "NSFWGateBypass" else # acronym needs special rule
-        if name == "banger" then "BANger" else
-        if name == "useQuickCss" then "useQuickCSS" else
-        if name == "webRichPresence" then "WebRichPresence (arRPC)" else
-        if builtins.elem name upperNames then unNixify name else
-        if builtins.elem name lowerPluginTitles then name else
-        if builtins.isAttrs value
-          && builtins.hasAttr "enable" value
-          && isLowerCamel name then toUpper name else
-        name
-      )
-      (
-        if builtins.isAttrs value then recurse value else # recurse into subsequent attrs
-        if builtins.elem value zeroOptions then 0 else # no concievable way to generalize
-        if builtins.elem value oneOptions then 1 else  # these without an upstream
-        if builtins.elem value twoOptions then 2 else  # change at Vencord since
-        if builtins.elem value threeOptions then 3 else# these settings enums are
-        if builtins.elem value fourOptions then 4 else # completely arbitrary
-        value
-      )
-    );
-  in recurse cfg;
+  mkVencordCfg =
+    cfg:
+    let
+      recurse = mapAttrs' (
+        name: value:
+        nameValuePair
+          (
+            # probably some kind of map function is better than this
+            if name == "enable" then
+              "enabled"
+            else if name == "tagSettings" then
+              "tagSettings"
+            # the only name that = attrset not in upperNames
+            else if name == "nsfwGateBypass" then
+              "NSFWGateBypass"
+            # acronym needs special rule
+            else if name == "banger" then
+              "BANger"
+            else if name == "useQuickCss" then
+              "useQuickCSS"
+            else if name == "webRichPresence" then
+              "WebRichPresence (arRPC)"
+            else if builtins.elem name upperNames then
+              unNixify name
+            else if builtins.elem name lowerPluginTitles then
+              name
+            else if builtins.isAttrs value && builtins.hasAttr "enable" value && isLowerCamel name then
+              toUpper name
+            else
+              name
+          )
+          (
+            if builtins.isAttrs value then
+              recurse value
+            # recurse into subsequent attrs
+            else if builtins.elem value zeroOptions then
+              0
+            # no concievable way to generalize
+            else if builtins.elem value oneOptions then
+              1
+            # these without an upstream
+            else if builtins.elem value twoOptions then
+              2
+            # change at Vencord since
+            else if builtins.elem value threeOptions then
+              3
+            # these settings enums are
+            else if builtins.elem value fourOptions then
+              4
+            # completely arbitrary
+            else
+              value
+          )
+      );
+    in
+    recurse cfg;
 in
 {
   inherit
