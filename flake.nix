5bafc700da8ee50c924c4549415b5c2ccc2c0d92
Benjamin Kaylor <blkaylor22@gmail.com>
2024-11-18 00:52:25 -0700

    treefmt format

--- flake.nix
+++ flake.nix
@@ -1,5 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-  outputs = inputs: { homeManagerModules.nixcord = import ./hm-module.nix; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+  };
+  outputs = { self, nixpkgs, systems, treefmt-nix, ... }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+    in {
+    homeManagerModules.nixcord = import ./hm-module.nix;
+  } // {
+      # formatter & check
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+      });
+    };
 }
