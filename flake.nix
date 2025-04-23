b958ea39d1c15d37bfd4bc9259931312017b624e
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-04-23 16:43:43 +0300

    flake.nix: `homeManagerModules` -> `homeModules`

--- flake.nix
+++ flake.nix
@@ -20,8 +20,15 @@
       treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in
     {
-      homeManagerModules.default = self.homeManagerModules.nixcord;
-      homeManagerModules.nixcord = import ./hm-module.nix;
+      homeManagerModules = nixpkgs.lib.mapAttrs (
+        name:
+        nixpkgs.lib.warn "Obsolete Flake attribute `nixcord.homeManagerModules.${name}' is used. It was renamed to `nixcord.homeModules.${name}`'."
+      ) self.homeModules;
+
+      homeModules = {
+        default = self.homeModules.nixcord;
+        nixcord = import ./hm-module.nix;
+      };
     }
     // {
       # formatter & check
