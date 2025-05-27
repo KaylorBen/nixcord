c8fa56548e8cd5ba4cffe4c61908c36ea2471d13
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-27 20:27:58 +0300

    flake.*: `nixpkgs-unstable` -> `nixos-25.05`, drop `treefmt-nix`

--- flake.nix
+++ flake.nix
@@ -1,40 +1,18 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      systems,
-      treefmt-nix,
-      ...
-    }:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  outputs = inputs: {
+    homeManagerModules = inputs.nixpkgs.lib.mapAttrs (
+      name:
+      inputs.nixpkgs.lib.warn "Obsolete Flake attribute `nixcord.homeManagerModules.${name}' is used. It was renamed to `nixcord.homeModules.${name}`'."
+    ) inputs.self.homeModules;
 
-      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-    in
-    {
-      homeManagerModules = nixpkgs.lib.mapAttrs (
-        name:
-        nixpkgs.lib.warn "Obsolete Flake attribute `nixcord.homeManagerModules.${name}' is used. It was renamed to `nixcord.homeModules.${name}`'."
-      ) self.homeModules;
-
-      homeModules = {
-        default = self.homeModules.nixcord;
-        nixcord = import ./hm-module.nix;
-      };
-    }
-    // {
-      # formatter & check
-      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-      checks = eachSystem (pkgs: {
-        formatting = treefmtEval.${pkgs.system}.config.build.check self;
-      });
+    homeModules = {
+      default = inputs.self.homeModules.nixcord;
+      nixcord = import ./hm-module.nix;
     };
+  };
 }
