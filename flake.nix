6f5594d70a37f8b7639ab333f85d067bae9f9a7c
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-31 21:15:39 +0300

    flake.*: switch to `flake-parts`, expose `discord`, `vencord` derivations

--- flake.nix
+++ flake.nix
@@ -1,18 +1,31 @@
 {
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
-  outputs = inputs: {
-    homeManagerModules = inputs.nixpkgs.lib.mapAttrs (
-      name:
-      inputs.nixpkgs.lib.warn "Obsolete Flake attribute `nixcord.homeManagerModules.${name}' is used. It was renamed to `nixcord.homeModules.${name}`'."
-    ) inputs.self.homeModules;
-
-    homeModules = {
-      default = inputs.self.homeModules.nixcord;
-      nixcord = import ./hm-module.nix;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          packages.discord = pkgs.callPackage ./discord.nix { };
+          packages.vencord = pkgs.callPackage ./vencord.nix { };
+        };
+      flake = {
+        homeModules = {
+          default = inputs.self.homeModules.nixcord;
+          nixcord = import ./hm-module.nix;
+        };
+      };
     };
-  };
 }
