d33f19468d3d69b7144b7cd84e2d3ed775d58fff
FlameFlag <github@flameflag.dev>
2025-12-22 23:32:35 +0200

    deprecated: clean up

--- modules/plugins/deprecated.nix
+++ modules/plugins/deprecated.nix
@@ -1,13 +1,1 @@
-{
-  USRBG = "usrbg";
-  XSOverlay = "xsOverlay";
-  autoDNDWhilePlaying = "autoDndWhilePlaying";
-  blurNSFW = "blurNsfw";
-  clearURLs = "clearUrLs";
-  copyUserURLs = "copyUserUrLs";
-  customRPC = "customRpc";
-  lastFMRichPresence = "lastFmRichPresence";
-  noRPC = "noRpc";
-  onePingPerDM = "onePingPerDm";
-  reviewDB = "reviewDb";
-}
+{ }
